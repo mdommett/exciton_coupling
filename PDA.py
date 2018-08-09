1b173287b1201e404a52678cbea50927c9661367
Michael Dommett <michaeldommett1@gmail.com>
2018-08-09 11:54:25 +0100

    COM function

--- PDA.py
+++ PDA.py
@@ -8,42 +8,40 @@ J = u1*u2/R12^5 - 3(u1*R12)(R12*u2) / R12^5
 where u1 and u2 are the TD vectors of the two systems and R12 is the vector
 between their centre of masses.
 """
-
-def centre_of_mass(symbols,x_coords,y_coords,z_coords):
+import numpy as np
+from periodic import element
+def centre_of_mass(symbols,coordinates):
     """
-    Calculates the centre of mass for a set of atomic positions
+    Calculates the centre of mass (COM) for a set of atomic positions, based on:
+
+    COM_x = m1x1 + m2x2 + ... + mnxn / m1 + m2 + ... + mn
+    COM_y = m1y1 + m2y2 + ... + mnyn / m1 + m2 + ... + mn
+    COM_z = m1z1 + m2z2 + ... + mnzn / m1 + m2 + ... + mn
 
     Parameters
     ----------
     symbols: List of strings
         List of elemental symbols
-    x_coords: List of floats
-        List of x-coordinate positions
-    y_coords: List of floats
-        List of y-coordinate positions
-    z_coords: List of floats
-        List of z-coordinate positions
-
+    coordinates: Nx3 array of floats
+        Array of x,y,z coordinates
     Returns
     ----------
-    Xcm,Ycm,Zcm: Floats of centre of mass of x,y,z coordinate
+    COM_x,COM_y,COM_z: Floats of centre of mass of x,y,z coordinate
     """
-    params=[symbols,x_coords,y_coords,z_coords]
-    n = len(params[0])
-    if all(len(x) != n for x in params):
-        raise ValueError("Inputs not of the same dimension!")
-    Xnumerator,Ynumerator,Znumerator,Xdenominator,Ydenominator,Zdenominator=0,0,0,0,0,0
+    if len(symbols)!=len(coordinates):
+        exit("Inputs not of the same dimension!")
 
-    for i in range(natoms):
-        Xnumerator += element(symbols[i]).mass*x_coords[i]
-        Ynumerator += element(symbols[i]).mass*y_coords[i]
-        Znumerator += element(symbols[i]).mass*z_coords[i]
-        Xdenominator += element(list_of_symbols[i]).mass
-        Ydenominator += element(list_of_symbols[i]).mass
-        Zdenominator += element(list_of_symbols[i]).mass
+    masses = np.array([element(i).mass for i in symbols])
+    mass_sum = np.sum(masses)
 
-    Xcm = Xnumerator/Xdenominator
-    Ycm = Ynumerator/Ydenominator
-    Zcm = Znumerator/Zdenominator
+    x_coords=coordinates[:,0]
+    y_coords=coordinates[:,1]
+    z_coords=coordinates[:,2]
+    x_numerator=np.sum(np.dot(masses,x_coords))
+    y_numerator=np.sum(np.dot(masses,y_coords))
+    z_numerator=np.sum(np.dot(masses,z_coords))
 
-    return Xcm,Ycm,Zcm
+    COM_x = x_numerator/mass_sum
+    COM_y = y_numerator/mass_sum
+    COM_z = z_numerator/mass_sum
+    return COM_x,COM_y,COM_z
