6d76489800ce098696661f72cc30228b59a850d8
Michael Dommett <michaeldommett1@gmail.com>
2018-08-13 11:51:36 +0100

    setup.py added

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='exciton_coupling',
+      version='0.1',
+      description='Exciton coupling analysis',
+      long_description=open('README.md').read(),
+      author='Michael Dommett, Rachel Crespo-Otero',
+      author_email='m.dommett@qmul.ac.uk@'
+      packages=['exciton_coupling',
+                'exciton_coupling.utils'])
